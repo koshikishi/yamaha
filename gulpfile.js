fd43f0a8910b93dbdd439cee7b106f367497991a
Anton Northern <48867587+koshikishi@users.noreply.github.com>
2019-09-23 12:37:06 +0300

    :tada: Создал новый проект

---
+++ gulpfile.js
@@ -0,0 +1,142 @@
+'use strict';
+
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cleancss = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var server = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+var imageminPngquant = require('imagemin-pngquant');
+var imageminMozjpeg = require('imagemin-mozjpeg');
+var svgstore = require('gulp-svgstore');
+var posthtml = require('gulp-posthtml');
+var include = require('posthtml-include');
+var htmlmin = require('gulp-htmlmin');
+var del = require('del');
+
+// Компиляция файлов *.css из *.scss с автопрефиксером и минификацией
+function css() {
+  return gulp.src('source/sass/style.scss')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: require('scss-resets').includePaths
+    }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cleancss())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/css'))
+    .pipe(server.stream());
+}
+
+// Запуск сервера Browsersync
+function server() {
+  server.init({
+    server: 'build/',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch('source/sass/**/*.{scss,sass}', css);
+  gulp.watch('source/img/icon-*.svg', gulp.series(gulp.parallel(sprite, html), refresh));
+  gulp.watch('source/*.html', gulp.series(html, refresh));
+}
+
+// Сжатие файлов изображений
+function img() {
+  return gulp.src('source/img/*.{png,jpg,svg}')
+    .pipe(imagemin([
+      imageminPngquant({
+        speed: 1,
+        strip: true,
+        quality: [0.7, 0.9]
+      }),
+      imageminMozjpeg({
+        quality: 75
+      }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest('build/img'));
+}
+
+// Сборка SVG-спрайта
+function sprite() {
+  return gulp.src('source/img/icon-*.svg')
+    .pipe(svgmin(function (file) {
+      var prefix = path.basename(file.relative, path.extname(file.relative));
+      return {
+        plugins: [{
+          cleanupIDs: {
+            prefix: prefix + '-',
+            minify: true
+          }
+        }]
+      }
+    }))
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/img'));
+}
+
+// Минификация файлов *.html
+function html() {
+  return gulp.src('source/*.html')
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('build'));
+}
+
+// Копирование файлов в папку build
+function copy() {
+  return gulp.src([
+      'source/fonts/**/*.{woff,woff2}',
+      'source/img/**',
+      'source/*.ico'
+    ], {
+      base: 'source'
+    })
+    .pipe(gulp.dest('build'));
+}
+
+// Удаление файлов в папке build перед копированием
+function clean() {
+  return del('build');
+}
+
+// Создание сборки проекта
+var build = gulp.series(clean, copy, gulp.parallel(css, sprite), html);
+
+// Автообновление страницы
+function refresh(done) {
+  server.reload();
+  done();
+}
+
+// Создание сборки проекта и запуск сервера Browsersync
+// var start = gulp.series(build, server);
+
+// Объявление задач
+exports.css = css;
+exports.server = server;
+exports.img = img;
+exports.sprite = sprite;
+exports.html = html;
+exports.copy = copy;
+exports.clean = clean;
+exports.build = build;
+exports.refresh = refresh;
+// exports.start = start;
